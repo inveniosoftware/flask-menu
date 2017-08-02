c11a677a358762d45019449f266035bf4de32c5a
Sebastian Witowski <witowski.sebastian@gmail.com>
2017-08-02 16:08:51 +0200

    general: fix failing tests

--- setup.py
+++ setup.py
@@ -123,8 +123,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Development Status :: 5 - Production/Stable'
     ],
