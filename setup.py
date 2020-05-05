d85ff7fa01a71c7e65e9d321012e66e2dd4f78a8
Diego Rodriguez <diego.rodriguez@cern.ch>
2020-05-05 12:31:58 +0200

    installation: remove Python 2.7 support
    * Closes #72.

--- setup.py
+++ setup.py
@@ -120,10 +120,9 @@ setup(
         'Framework :: Flask',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Development Status :: 5 - Production/Stable'
     ],
     cmdclass={'test': PyTest},
