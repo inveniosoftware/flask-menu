5bbf0d3bc20fab6fd784124e7ee50ae228b30723
Parth Shandilya <parth.shandilya@cern.ch>
2020-12-08 10:56:31 +0100

    global: migrate CI to gh-actions
    closes #76

--- run-tests.sh
+++ run-tests.sh
@@ -1,15 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
 #
 # This file is part of Flask-Menu
-# Copyright (C) 2013, 2014, 2015 CERN.
+# Copyright (C) 2013-2020 CERN.
 #
 # Flask-Menu is free software; you can redistribute it and/or modify
 # it under the terms of the Revised BSD License; see LICENSE file for
 # more details.
 
-pydocstyle flask_menu && \
-isort -c -c -df **/*.py && \
-check-manifest --ignore ".travis-*" --ignore docs/_themes && \
-sphinx-build -qnNW docs docs/_build/html && \
-python setup.py test && \
-sphinx-build -qnNW -b doctest docs docs/_build/doctest
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+python -m check_manifest --ignore ".*-requirements.txt" --ignore docs/_themes
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
+python -m pytest
+python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
