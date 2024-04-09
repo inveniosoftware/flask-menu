862ea808662ad8f29a23a7d5917db60a07c2f7c0
Martin Obersteiner <obersteiner@tugraz.at>
2024-04-09 10:26:31 +0200

    config: add config-variable for setting menus

---
+++ flask_menu/config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Flask-Menu
+# Copyright (C) 2024 Graz University of Technology.
+#
+# Flask-Menu is free software; you can redistribute it and/or modify
+# it under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+
+"""Default configuration for Flask-Menu."""
+
+FLASK_MENU = {}
+"Make menus setable via config."
